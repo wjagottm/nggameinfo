a4ce8bd618e35e8289e6350c32e63f0723a64ef9
wjagottm <wjagottm@avans.nl>
2018-12-12 12:57:07 +0100

    fixed deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/gameinfo'));
+app.use(express.static('./dist/gameinfo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/gameinfo/index.html'));
+res.sendFile(path.join(__dirname,'/dist/gameinfo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
